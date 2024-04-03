8300e53852b2fe5856272e6d111822e350ed3a8e
screret <68943070+screret@users.noreply.github.com>
2024-04-03 13:30:39 +0300

    fix OneTimeEventReceiver waitingModListeners only allowing 1 listener per class

--- src/main/java/com/tterrag/registrate/util/OneTimeEventReceiver.java
+++ src/main/java/com/tterrag/registrate/util/OneTimeEventReceiver.java
@@ -6,21 +6,15 @@ import com.tterrag.registrate.AbstractRegistrate;
 import com.tterrag.registrate.util.nullness.NonnullType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import net.neoforged.bus.EventBus;
-import net.neoforged.bus.ListenerList;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.event.IModBusEvent;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.neoforged.fml.util.ObfuscationReflectionHelper;
 import net.neoforged.neoforge.common.NeoForge;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 
-import javax.annotation.Nullable;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -30,7 +24,6 @@ import java.util.function.Consumer;
 @Log4j2
 public class OneTimeEventReceiver<T extends Event> implements Consumer<@NonnullType T> {
 
-    private static final Table<AbstractRegistrate<?>, Class<?>, Pair<EventPriority, Consumer<?>>> WAITING_MOD_LISTENERS = HashBasedTable.create();
 
     public static <T extends Event & IModBusEvent> void addModListener(AbstractRegistrate<?> owner, Class<? super T> evtClass, Consumer<? super T> listener) {
         OneTimeEventReceiver.<T>addModListener(owner, EventPriority.NORMAL, evtClass, listener);
@@ -38,14 +31,19 @@ public class OneTimeEventReceiver<T extends Event> implements Consumer<@NonnullT
     
     public static <T extends Event & IModBusEvent> void addModListener(AbstractRegistrate<?> owner, EventPriority priority, Class<? super T> evtClass, Consumer<? super T> listener) {
         if (owner.getModEventBus() == null) {
-            WAITING_MOD_LISTENERS.put(owner, evtClass, Pair.of(priority, listener));
+            if (!waitingModListeners.contains(owner, evtClass)) {
+                waitingModListeners.put(owner, evtClass, new ArrayList<>());
+            }
+            waitingModListeners.get(owner, evtClass).add(Pair.of(priority, listener));
             return;
         }
         if (!seenModBus) {
             seenModBus = true;
-            for (var waitingListener : WAITING_MOD_LISTENERS.row(owner).entrySet()) {
-                //noinspection unchecked
-                OneTimeEventReceiver.<T>addListener(owner.getModEventBus(), waitingListener.getValue().getKey(), (Class<? super T>) waitingListener.getKey(), (Consumer<? super T>) waitingListener.getValue().getValue());
+            for (var waitingListener : waitingModListeners.row(owner).entrySet()) {
+                for (var pair : waitingListener.getValue()) {
+                    //noinspection unchecked
+                    OneTimeEventReceiver.<T>addListener(owner.getModEventBus(), pair.getKey(), (Class<? super T>) waitingListener.getKey(), (Consumer<? super T>) pair.getValue());
+                }
             }
             addModListener(owner, FMLLoadCompleteEvent.class, OneTimeEventReceiver::onLoadComplete);
         }
@@ -72,19 +70,7 @@ public class OneTimeEventReceiver<T extends Event> implements Consumer<@NonnullT
     }
 
     private static boolean seenModBus = false;
-    @Nullable
-    private static final MethodHandle getListenerList;
-
-    static {
-        MethodHandle ret;
-        try {
-            ret = MethodHandles.lookup().unreflect(ObfuscationReflectionHelper.findMethod(EventBus.class, "getListenerList", Class.class));
-        } catch (IllegalAccessException e) {
-            log.warn("Failed to set up EventBus reflection to release one-time event listeners, leaks will occur. This is not a big deal.");
-            ret = null;
-        }
-        getListenerList = ret;
-    }
+    private static final Table<AbstractRegistrate<?>, Class<?>, List<Pair<EventPriority, Consumer<?>>>> waitingModListeners = HashBasedTable.create();
 
     private final IEventBus bus;
     private final Consumer<? super T> listener;
@@ -114,17 +100,7 @@ public class OneTimeEventReceiver<T extends Event> implements Consumer<@NonnullT
 
     private static void onLoadComplete(FMLLoadCompleteEvent event) {
         event.enqueueWork(() -> {
-            toUnregister.forEach(t -> {
-                t.getLeft().unregister(t.getMiddle());
-                try {
-                    if (getListenerList != null) {
-                        ListenerList list = (ListenerList) getListenerList.invokeExact((EventBus)t.getLeft(), t.getRight());
-                        list.getListeners();
-                    }
-                } catch (Throwable ex) {
-                    log.warn("Failed to clear listener list of one-time event receiver, so the receiver has leaked. This is not a big deal.", ex);
-                }
-            });
+            toUnregister.forEach(t -> t.getLeft().unregister(t.getMiddle()));
             toUnregister.clear();
         });
     }
